8a4e2ec41a33d52cc262e013e72e20f53746967f
vishvakbandi <vishvakbandi@gmail.com>
2021-05-27 16:52:38 -0500

    delete test file

--- main.js
+++
@@ -1,9 +0,0 @@
-// import { loginEmail } from "./middleware/authentication";
-
-const auth = require("./middleware/authentication");
-
-console.log("Hello world");
-
-// node
-auth.initFirebase();
-console.log(auth.loginWithEmail("test@mail.com", "password"));
