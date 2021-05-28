5c03e8c1afda6383f1281ce22fd5b171b48e7f7d
amertx <amerdtx@gmail.com>
2021-05-27 21:36:02 -0500

    added credit

---
+++ apiTesting.py
@@ -0,0 +1,4 @@
+import requests
+import json
+x = requests.post("http://api.reimaginebanking.com/enterprise/accounts?key=http://api.nessieisreal.com/customers?key=d5d9ca8cf8097e12542b93cc06157453")
+print(x.text)
